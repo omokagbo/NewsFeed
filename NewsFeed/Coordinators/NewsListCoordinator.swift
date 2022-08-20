fba1ee032913bdeab54678e7954463f18bc34138
omokagbo <emmanuel.omokagbo@gmail.com>
2022-08-20 22:52:52 +0100

    chore: add layout extension

--- NewsFeed/Coordinators/NewsListCoordinator.swift
+++ NewsFeed/Coordinators/NewsListCoordinator.swift
@@ -26,7 +26,7 @@ final class NewsListCoordinator: BaseCoordinator {
         navigationController?.pushViewController(vc, animated: true)
     }
     
-    fileprivate func showNewsDetailsFor(_ news: News) {
+    func showNewsDetailsFor(_ news: News) {
         let vc = AppDIContainer.makeNewsDetailsController()
         vc.news = news
         navigationController?.pushViewController(vc, animated: true)
