cbbe9c6a1396d390d4027ba403d338e92ae5bdd6
omokagbo <emmanuel.omokagbo@gmail.com>
2022-08-20 12:16:16 +0100

    Initial Commit

---
+++ NewsFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+// ViewController.swift
+// NewsFeed
+
+//  Created by Emmanuel Omokagbo on 20/08/2022
+//  Copyright © 2022 Emmanuel Omokagbo. All rights reserved.
+	
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
