46a906700914db0a3fa08bc3ec4da2c40f49a73b
omokagbo <emmanuel.omokagbo@gmail.com>
2022-08-20 12:31:59 +0100

    chore: create project and add folders

--- NewsFeed/Features/NewsList/NewsListController.swift
+++ NewsFeed/Features/NewsList/NewsListController.swift
@@ -4,17 +4,18 @@
 
 //  Created by Emmanuel Omokagbo on 20/08/2022
 //  Copyright © 2022 Emmanuel Omokagbo. All rights reserved.
-	
+
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class NewsListController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "News"
+        view.backgroundColor = .systemBackground
     }
-
-
+    
+    
 }
 
