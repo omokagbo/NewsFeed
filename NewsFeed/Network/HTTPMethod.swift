e75395041a410c884c09c894845a1f10bca10cff
omokagbo <emmanuel.omokagbo@gmail.com>
2022-08-20 13:22:16 +0100

    chore: add network folder

---
+++ NewsFeed/Network/HTTPMethod.swift
@@ -0,0 +1,18 @@
+//
+// HTTPMethod.swift
+// NewsFeed
+
+//  Created by Emmanuel Omokagbo on 20/08/2022
+//  Copyright © 2022 Emmanuel Omokagbo. All rights reserved.
+	
+
+import Foundation
+
+public enum HTTPMethod: String {
+    case get     = "GET"
+    case head    = "HEAD"
+    case post    = "POST"
+    case put     = "PUT"
+    case patch   = "PATCH"
+    case delete  = "DELETE"
+}
